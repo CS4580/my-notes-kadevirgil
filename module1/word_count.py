f225ec3ce991e68fd6bd26af18030dba507be4c1
Kade Virgil <virgilkade1@gmail.com>
2024-08-28 10:19:44 -0600

    web file reading

---
+++ module1/word_count.py
@@ -0,0 +1,34 @@
+""" Counting the amount of words from an online .txt file
+"""
+from urllib.request import urlopen
+
+def word_count(address):
+    """ Calculates the numher of words in a file given an address
+
+    Args:
+        address (str): the path to the web file
+
+    Returns:
+        int: returns the number of words
+    """
+    # Read file from web
+    temp = ""
+    with urlopen(address) as data:
+        for line in data:
+            temp += line.decode('utf-8')
+
+        words = temp.split()
+        word_count = len(words)
+        
+    return word_count
+
+def main():
+    """Driven Function
+    """
+    file_addr = 'http://icarus.cs.weber.edu/~hvalle/sample_data/poem.txt'
+    count = word_count(file_addr)
+    print(f'The number of words in the file is: {count}')
+
+
+if __name__ == '__main__':
+    main()
