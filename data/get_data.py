d73e3ecf82cf98b00f20ba27560ded200f0f8cd2
Kade Virgil <virgilkade1@gmail.com>
2024-10-14 10:08:10 -0600

    module6 notes

--- data/get_data.py
+++ data/get_data.py
@@ -3,17 +3,30 @@
 
 # Global Variables
 SERVER_URL = "http://icarus.cs.weber.edu/~hvalle/cs4580/data/"
-FILE_NAME = "plottingDemo01.zip"
+FILE_NAME = "seaborData.zip"
 KAGGLE_DATASET_NAME = ""
 
 import os
 import requests
+import argparse
 from zipfile import ZipFile, is_zipfile
+import kaggle
 
-# TODO: Crate another function to download the files 
-# frome kaggle directly by passing the dataset name
-def download_kaggle_data(dataset_name): 
-    pass
+# TODO: Create another function to download the files 
+# from Kaggle directly by passing the dataset name
+def download_kaggle_data(dataset_name):
+    """Downloads a dataset from Kaggle.
+
+    Args:
+        dataset_name (str): Name of the Kaggle dataset to download.
+    """
+
+    # Authenticate using the Kaggle API
+    kaggle.api.authenticate()
+
+    # Download the dataset
+    kaggle.api.dataset_download_files(dataset_name, path='data', unzip=True)
+    print(f"Kaggle dataset downloaded and unzipped successfully: {dataset_name}")
 
 def download_file(url, file_name):
     """Downloads a file from a URL and optionally unzips it if it is a ZIP file.
@@ -34,7 +47,7 @@ def download_file(url, file_name):
             file.write(response.content)
         print(f"File downloaded successfully: {file_name}")
 
-        # Check if the file is a ZIP file, if True unzippes the download file into the pwd
+        # Check if the file is a ZIP file, if True unzips the downloaded file into the 'data' directory
         if is_zipfile(file_name):
             unzip_file(file_name)
     else:
@@ -51,13 +64,21 @@ def unzip_file(file_name):
         # Extract all the contents to the data directory
         zip_ref.extractall('data')
         print(f"File unzipped successfully: {file_name}")
+        # Print all the names of the files extracted
+        for file in zip_ref.namelist():
+            print(f"Extracted: {file}")
     os.remove(file_name)
 
 
-def main(server_url, file_name):
+def main():
     """Driven function"""
-    download_file(server_url, file_name)
+    parser = argparse.ArgumentParser(description="Download data from a server.")
+    parser.add_argument('--server_url', type=str, default=SERVER_URL, help='URL of the server to download data from')
+    parser.add_argument('--file_name', type=str, default=FILE_NAME, help='Name of the file to download')
+    args = parser.parse_args()
+
+    download_file(args.server_url, args.file_name)
 
 
 if __name__ == "__main__":
-    main(SERVER_URL, FILE_NAME)
+    main()
